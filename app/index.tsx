642ff5cb1920995c8aec4626a6d91bf93e1d796d
cecile hirschauer <cecile.h.75@gmail.com>
2024-08-06 10:46:24 +0200

    setup ract native project

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
+
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World !</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+})
