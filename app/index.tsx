98b3bdc23cc6f731f01c4df42add5b00a233dccb
cecile hirschauer <cecile.h.75@gmail.com>
2024-08-06 11:00:43 +0200

    setup nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Image, StyleSheet, Platform, View, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center">
       <Text>Hello World !</Text>
     </View>
   )
